82aaed13e56af9f150ca35e8f2a67d50bd41c742
m0rv4i <20513519+m0rv4i@users.noreply.github.com>
2020-12-06 17:17:57 +0000

    Add sonar auth

--- vars/runMsbuildWithSonar.groovy
+++ vars/runMsbuildWithSonar.groovy
@@ -6,7 +6,8 @@ def call(String configuration = "Release", String platform = "Any CPU", String s
     def sonar_msbuild_location = tool sonarScannerInstance
 
     withSonarQubeEnv(sonarInstance) {
-        bat "${sonar_msbuild_location}\\SonarScanner.MSBuild.exe begin -k:\"${JOB_NAME}\" -d:sonar.host.url=${env.SONAR_HOST_URL}"
+        def sonar_token = System.getenv("SONAR_TOKEN")
+        bat "${sonar_msbuild_location}\\SonarScanner.MSBuild.exe begin -k:\"${JOB_NAME}\" -d:sonar.host.url=${env.SONAR_HOST_URL} -d:sonar.login=${sonar_token}"
         runMsbuild(configuration, platform)
         bat "${sonar_msbuild_location}\\SonarScanner.MSBuild.exe end"
     }
--- vars/sonarOnDocker.groovy
+++ vars/sonarOnDocker.groovy
@@ -2,6 +2,7 @@
 
 def call(String sonarInstance = "Sonarqube"){
     withSonarQubeEnv(sonarInstance) {
-        bat "docker run --rm -e SONAR_HOST_URL=\"${env.SONAR_HOST_URL}\" -v \"%cd%:/usr/src\" sonarsource/sonar-scanner-cli:latest -Dsonar.projectKey=${JOB_NAME}"
+        def sonar_token = System.getenv("SONAR_TOKEN")
+        bat "docker run --rm -e SONAR_HOST_URL=\"${env.SONAR_HOST_URL}\" -v \"%cd%:/usr/src\" sonarsource/sonar-scanner-cli:latest -Dsonar.projectKey=${JOB_NAME} -Dsonar.login=${sonar_token}"
     }
 }
