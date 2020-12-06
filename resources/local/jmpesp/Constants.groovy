1960a5d6ef8a80f6c730cd927d5c1a964d574220
m0rv4i <20513519+m0rv4i@users.noreply.github.com>
2020-12-06 13:13:36 +0000

    Fix constants

--- resources/local/jmpesp/Constants.groovy
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env groovy
-
-package local.jmpesp
-
-class Constants {
-
-    def sonar_msbuild_location = "C:\\tools\\sonar-scanner-msbuild-5.0.4.24009-net46\\SonarScanner.MSBuild.exe"
-
-}
--- vars/msbuildWithSonar.groovy
+++ vars/msbuildWithSonar.groovy
@@ -1,11 +1,11 @@
 #!/usr/bin/env groovy
 
-import local.jmpesp.Constants
+static String sonar_msbuild_location = "C:\\tools\\sonar-scanner-msbuild-5.0.4.24009-net46\\SonarScanner.MSBuild.exe"
 
 def call(String configuration = "Release", String platform = "Any CPU", String sonarInstance = "Sonarqube"){
     withSonarQubeEnv("${sonarInstance}") {
-        bat "${Constants.sonar_msbuild_location} begin -k:\"${JOB_NAME}\" -d:sonar.host.url=${env.SONAR_HOST_URL}"
+        bat "${sonar_msbuild_location} begin -k:\"${JOB_NAME}\" -d:sonar.host.url=${env.SONAR_HOST_URL}"
         msbuild(configuration, platform)
-        bat "${Constants.sonar_msbuild_location} end"
+        bat "${sonar_msbuild_location} end"
     }
 }
