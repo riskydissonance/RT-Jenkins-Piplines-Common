1cab434a719b83cac5b2851b312857738b1653e5
m0rv4i <20513519+m0rv4i@users.noreply.github.com>
2020-12-06 15:03:49 +0000

    Rename to avoid clashes

--- vars/runMsbuild.groovy
+++ vars/runMsbuild.groovy
@@ -1,5 +1,6 @@
 #!/usr/bin/env groovy
 
 def call(String configuration = "Release", String platform = "Any CPU"){
+    bat 'nuget restore SolutionName.sln'
     bat "\"${tool 'MSBuild'}\" ${JOB_NAME}.sln /p:Configuration=${configuration} /p:Platform=\"${platform}\""
 }
