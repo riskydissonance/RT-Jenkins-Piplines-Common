09fb6e0524e1f0534e43ba8f297645cda59e89e7
m0rv4i <20513519+m0rv4i@users.noreply.github.com>
2020-12-06 13:17:04 +0000

    Move variable

--- vars/msbuildWithSonar.groovy
+++ vars/msbuildWithSonar.groovy
@@ -1,8 +1,9 @@
 #!/usr/bin/env groovy
 
-static String sonar_msbuild_location = "C:\\tools\\sonar-scanner-msbuild-5.0.4.24009-net46\\SonarScanner.MSBuild.exe"
-
 def call(String configuration = "Release", String platform = "Any CPU", String sonarInstance = "Sonarqube"){
+
+    def sonar_msbuild_location = "C:\\tools\\sonar-scanner-msbuild-5.0.4.24009-net46\\SonarScanner.MSBuild.exe"
+
     withSonarQubeEnv("${sonarInstance}") {
         bat "${sonar_msbuild_location} begin -k:\"${JOB_NAME}\" -d:sonar.host.url=${env.SONAR_HOST_URL}"
         msbuild(configuration, platform)
