ed50da0dd70ca8dee465cf17645d2b35aca2cd2c
m0rv4i <20513519+m0rv4i@users.noreply.github.com>
2020-12-06 15:13:25 +0000

    Fix Sonar

--- vars/runMsbuildWithSonar.groovy
+++ vars/runMsbuildWithSonar.groovy
@@ -6,8 +6,8 @@ def call(String configuration = "Release", String platform = "Any CPU", String s
     def sonar_msbuild_location = tool sonarScannerInstance
 
     withSonarQubeEnv(sonarInstance) {
-        bat "${sonar_msbuild_location} begin -k:\"${JOB_NAME}\" -d:sonar.host.url=${env.SONAR_HOST_URL}"
+        bat "${sonar_msbuild_location}\\SonarScanner.MSBuild.exe begin -k:\"${JOB_NAME}\" -d:sonar.host.url=${env.SONAR_HOST_URL}"
         runMsbuild(configuration, platform)
-        bat "${sonar_msbuild_location} end"
+        bat "${sonar_msbuild_location}\\SonarScanner.MSBuild.exe end"
     }
 }
