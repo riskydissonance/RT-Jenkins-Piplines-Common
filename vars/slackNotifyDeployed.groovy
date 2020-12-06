f9718abdbda55c2dc73ee18158bcfdd0665d1fb5
m0rv4i <20513519+m0rv4i@users.noreply.github.com>
2020-12-06 16:47:16 +0000

    Remove user from slack message

--- vars/slackNotifyDeployed.groovy
+++ vars/slackNotifyDeployed.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
 
 def call(String repo){
-    slackNotify("${env.BUILD_USER_ID} deployed ${JOB_NAME} for ${params.ProjectID} to <${env.BUILD_URL}https://artifactory.jmpesp.local/engagements/${params.ProjectID}/${JOB_NAME}/|artifactory>")
+    slackNotify("${JOB_NAME} for ${params.ProjectID} to <${env.BUILD_URL}https://artifactory.jmpesp.local/engagements/${params.ProjectID}/${JOB_NAME}/|artifactory>")
 }
--- vars/slackNotifyPipelineFailed.groovy
+++ vars/slackNotifyPipelineFailed.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
 
 def call(){
-    slackNotifyFailure("${JOB_NAME} deploy failed for ${params.ProjectID} by ${env.BUILD_USER_ID}: *${env.DEPLOY_VERSION}*. <${env.BUILD_URL}|Check build>")
+    slackNotifyFailure("${JOB_NAME} deploy failed for ${params.ProjectID}: *${env.DEPLOY_VERSION}*. <${env.BUILD_URL}|Check build>")
 }
