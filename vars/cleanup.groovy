027d93791b21e1a12799964acec754916711cbb0
m0rv4i <20513519+m0rv4i@users.noreply.github.com>
2020-12-06 17:46:44 +0000

    Full cleanup

--- vars/cleanup.groovy
+++ vars/cleanup.groovy
@@ -1,5 +1,21 @@
 #!/usr/bin/env groovy
 
 def call(){
+    // clean up our workspace
      deleteDir()
+
+     // clean up tmp directory
+     dir("${workspace}@tmp") {
+          deleteDir()
+     }
+
+     // clean up script directory
+     dir("${workspace}@script") {
+          deleteDir()
+     }
+
+     // clean up libs directory
+     dir("${workspace}@libs") {
+          deleteDir()
+     }
 }
