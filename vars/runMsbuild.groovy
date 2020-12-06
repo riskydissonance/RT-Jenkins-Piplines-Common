976f10043bced212ee3b3f9b8fbcbed599167946
m0rv4i <20513519+m0rv4i@users.noreply.github.com>
2020-12-06 15:10:47 +0000

    Fix msbuild

--- vars/runMsbuild.groovy
+++ vars/runMsbuild.groovy
@@ -4,8 +4,9 @@ def call(String configuration = "Release", String platform = "Any CPU"){
 
     try {
         bat "nuget restore ${JOB_NAME}.sln"
-    }(Exception ex) {
-        echo "Error running nuget restore, trying to continue...";
+    } catch (Exception ex) {
+        echo "Error running nuget restore: " + ex.message;
+        echo "Trying to continue..."
     }
 
     bat "\"${tool 'MSBuild'}\" ${JOB_NAME}.sln /p:Configuration=${configuration} /p:Platform=\"${platform}\""
