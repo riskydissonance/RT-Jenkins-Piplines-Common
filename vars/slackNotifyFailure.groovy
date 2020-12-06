c50cdaf84fa0da288b9ea11a6324c0d1c77f0298
m0rv4i <20513519+m0rv4i@users.noreply.github.com>
2020-12-06 16:55:49 +0000

    Changes

--- vars/slackNotifyFailure.groovy
+++ vars/slackNotifyFailure.groovy
@@ -1,13 +1,11 @@
 #!/usr/bin/env groovy
 
 def call(String message){
-    if("${params.ProjectID}" != "No Project"){
-        slackSend(
-            teamDomain: System.getenv("SLACK_DOMAIN"),
-            token: System.getenv("SLACK_TOKEN"),
-            channel: "#dev-pipeline-failures",
-            color: "danger",
-            message: message
-        )
-    }
+    slackSend(
+        teamDomain: System.getenv("SLACK_DOMAIN"),
+        token: System.getenv("SLACK_TOKEN"),
+        channel: "#dev-pipeline-failures",
+        color: "danger",
+        message: message
+    )
 }
