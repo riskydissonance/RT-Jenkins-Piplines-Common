0d1772cc091e52eb84c1c87ba88cbfac03172b6d
m0rv4i <20513519+m0rv4i@users.noreply.github.com>
2021-01-02 23:01:51 +0000

    Yes

--- vars/runMsbuildWithSonar.groovy
+++ vars/runMsbuildWithSonar.groovy
@@ -1,14 +1,14 @@
 #!/usr/bin/env groovy
 
 
-def call(String configuration = "Release", String platform = "Any CPU", String sonarInstance = "Sonarqube", String sonarScannerInstance = "SonarScannerMSBuild"){
+def call(String configuration = "Release", String platform = "Any CPU", String targetFrameworkVersion="v4.5", String sonarInstance = "Sonarqube", String sonarScannerInstance = "SonarScannerMSBuild"){
 
     def sonar_msbuild_location = tool sonarScannerInstance
 
     withSonarQubeEnv(sonarInstance) {
         def sonar_token = System.getenv("SONAR_TOKEN")
         bat "${sonar_msbuild_location}\\SonarScanner.MSBuild.exe begin -k:\"${JOB_NAME}\" -d:sonar.host.url=${env.SONAR_HOST_URL} -d:sonar.login=${sonar_token}"
-        runMsbuild(configuration, platform)
+        runMsbuild(configuration, platform, targetFrameworkVersion)
         bat "${sonar_msbuild_location}\\SonarScanner.MSBuild.exe end -d:sonar.login=${sonar_token}"
     }
 }
